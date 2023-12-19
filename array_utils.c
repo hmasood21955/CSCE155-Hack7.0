d4c8f993cb1f1b8df637c60f10df6576c7e521b6
hmasood21955 <hmasood878@gmail.com>
2023-12-19 21:36:48 +0500

    contain  program

--- array_utils.c
+++ array_utils.c
@@ -1,5 +1,13 @@
 #include "array_utils.h"
 #include<stdlib.h>
+int contains(const int *arr, int size, int x) {
+    for (int i = 0; i < size; ++i) {
+        if (arr[i] == x) {
+            return 1;  
+        }
+    }
+    return 0;  
+}
 
 
 //place your function definitions here

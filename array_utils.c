1a519634761984e5496bc887a4a2e7a6b9d1a352
hmasood21955 <hmasood878@gmail.com>
2023-12-19 21:54:35 +0500

    reverse funtion

--- array_utils.c
+++ array_utils.c
@@ -38,3 +38,11 @@ int *paddedCopy(const int *arr, int oldSize, int newSize) {
 
     return newArr;
 }
+void reverse(int *arr, int size) {
+    for (int i = 0; i < size / 2; ++i) {
+        int temp = arr[i];
+        arr[i] = arr[size - 1 - i];
+        arr[size - 1 - i] = temp;
+    }
+}
+

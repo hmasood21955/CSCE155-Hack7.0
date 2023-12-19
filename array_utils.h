6fec3d1a51d0770931613b9e57d07821985369e4
hmasood21955 <hmasood878@gmail.com>
2023-12-19 21:17:00 +0500

    containsWithin

--- array_utils.h
+++ array_utils.h
@@ -2,5 +2,6 @@
 #define ARRAY_UTILS_H
 
 int contains(const int *arr, int size, int x);
+int containsWithin(const int *arr, int size, int x, int i, int j);
 
 //place your documentation and prototypes here

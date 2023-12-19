7e5d2d56df01b6a8691267c7967cd5bd26b21b7a
hmasood21955 <hmasood878@gmail.com>
2023-12-19 21:19:01 +0500

    *reverseCopy

--- array_utils.h
+++ array_utils.h
@@ -5,5 +5,8 @@ int contains(const int *arr, int size, int x);
 int containsWithin(const int *arr, int size, int x, int i, int j);
 int *paddedCopy(const int *arr, int oldSize, int newSize);
 void reverse(int *arr, int size);
+int *reverseCopy(const int *arr, int size);
+
+#endif  // ARRAY_UTILS_H
 
 //place your documentation and prototypes here

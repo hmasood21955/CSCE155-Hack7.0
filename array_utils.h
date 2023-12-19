a197881c4e415f469f17891ac56b381027442c51
hmasood21955 <hmasood878@gmail.com>
2023-12-19 21:17:47 +0500

    *paddedCopy

--- array_utils.h
+++ array_utils.h
@@ -3,5 +3,6 @@
 
 int contains(const int *arr, int size, int x);
 int containsWithin(const int *arr, int size, int x, int i, int j);
+int *paddedCopy(const int *arr, int oldSize, int newSize);
 
 //place your documentation and prototypes here
